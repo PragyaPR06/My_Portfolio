9cf9622b688ae19642312cd969f7a7e4e08d33b1
pragatikalwar <129779922+pragatikalwar@users.noreply.github.com>
2025-09-15 20:46:40 +0530

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 import validator from 'validator'
 
 export function Form() {
-  const [state, handleSubmit] = useForm('xpzgpkoo')
+  const [state, handleSubmit] = useForm('xovnzlwk')
   const [validEmail, setValidEmail] = useState(false)
   // const [isHuman, setIsHuman] = useState(false)
   const [message, setMessage] = useState('')
